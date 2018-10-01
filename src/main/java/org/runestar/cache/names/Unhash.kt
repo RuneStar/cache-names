237bd5e9501864e3ae0f712fee017ea21c7d77b4
hunterwb <hunter@hunterwb.com>
2018-10-01 03:53:34 -0700

    Add new unhash method

--- src/main/java/org/runestar/cache/names/Unhash.kt
+++ src/main/java/org/runestar/cache/names/Unhash.kt
@@ -269,3 +269,54 @@ fun unhash(
     pool.shutdown()
     pool.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS)
 }
+
+fun unhash(
+        results: WritableByteChannel,
+        dict: Set<String>,
+        separator: Char,
+        targetHashes: IntSet,
+        maxCombinations: Int
+) {
+    val separatorByte = separator.toString().toByteArray(CHARSET).first()
+    val dictArray = dict.map { it.toByteArray(CHARSET) }.toTypedArray()
+    val maxWordLength = dictArray.maxBy { it.size }!!.size
+    val n = dictArray.size
+
+    val pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() - 1)
+
+    for (startIndex in 0 until n) {
+        pool.submit {
+            val curHashes = IntArray(maxCombinations)
+            val writeBuf = ByteBuffer.allocate((maxWordLength + 1) * maxCombinations + 1)
+            multisetPermutations(n * 2, maxCombinations, intArrayOf(startIndex)) { indices, len ->
+                val v = indices[len - 1]
+                val lastHash = curHashes[len - 1]
+                val hash = if (v >= n) {
+                    lastHash.update(separatorByte).update(dictArray[v - n])
+                } else {
+                    lastHash.update(dictArray[v])
+                }
+                if (len != maxCombinations) {
+                    curHashes[len] = hash
+                }
+                if (hash in targetHashes) {
+                    writeBuf.clear()
+                    for (i in 0 until len) {
+                        val vv = indices[i]
+                        if (vv >= n) {
+                            writeBuf.put(separatorByte).put(dictArray[vv - n])
+                        } else {
+                            writeBuf.put(dictArray[vv])
+                        }
+                    }
+                    writeBuf.put('\n'.toByte())
+                    writeBuf.flip()
+                    results.write(writeBuf)
+                }
+            }
+        }
+    }
+
+    pool.shutdown()
+    pool.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS)
+}

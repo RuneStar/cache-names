b1c9b0725d792c95684eed6cf97d67205ddec69b
hunterwb <hunter@hunterwb.com>
2018-10-01 03:21:16 -0700

    Remove unused combination methods

--- src/main/java/org/runestar/cache/names/Combinations.kt
+++ src/main/java/org/runestar/cache/names/Combinations.kt
@@ -1,50 +1,6 @@
 package org.runestar.cache.names
 
-import java.util.*
 import kotlin.math.max
-
-inline fun permutations(length: Int, action: (IntArray) -> Unit) {
-    val objs = IntArray(length) { it }
-    val c = IntArray(length)
-    action(objs)
-    var plus = false
-    var i = 0
-    while (i < length) {
-        if (c[i] < i) {
-            if (i % 2 == 0) {
-                objs.swap(0, i)
-            } else {
-                objs.swap(c[i], i)
-            }
-            action(objs)
-            plus = !plus
-            c[i]++
-            i = 0
-        } else {
-            c[i] = 0
-            i++
-        }
-    }
-}
-
-fun IntArray.swap(indexA: Int, indexB: Int) {
-    val a = get(indexA)
-    set(indexA, get(indexB))
-    set(indexB, a)
-}
-
-inline fun subsets(n: Int, k: Int, b: BitSet, action: (BitSet) -> Unit) {
-    b.clear()
-    b.set(0, k)
-    while (!b.get(n)) {
-        action(b)
-        val lo = b.nextSetBit(0)
-        val lz = b.nextClearBit(lo)
-        b.set(lz)
-        b.clear(0, lz)
-        b.set(0, lz - lo - 1)
-    }
-}
 
 inline fun multisetPermutations(
         n: Int,

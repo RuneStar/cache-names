42ca6c5cb77b5c2b3f37997260290c114e728249
hunterwb <hunter@hunterwb.com>
2019-06-24 14:45:02 -0700

    Add graphics
    From chrisl

--- src/main/java/org/runestar/cache/names/MergeFiles.kt
+++ src/main/java/org/runestar/cache/names/MergeFiles.kt
@@ -142,6 +142,7 @@ fun main() {
         "v2_stone_borders_alt,$x".add()
         "favourite_icons,$x".add()
         "mod_icons_interface,$x".add()
+        "spectator,$x".add()
     }
 
     val knownNamesMap = knownNames.associateBy { it.hashCode() }
